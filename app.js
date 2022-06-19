95740c2addff88755b844c58ec311676a18d8d3a
leode <leodefarias25@gmail.com>
2022-06-19 13:35:31 -0500

    added all files and modifications

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+
+
+var zoom = 1;
+var width = 100;
+
+function bigger() {
+    zoom = zoom + 0.1;
+    width = 100 / zoom;
+    document.body.style.transformOrigin = "left top";
+    document.body.style.transform = "scale(" + zoom + ")";
+    document.body.style.width = width + "%";
+}
+function smaller() {
+    zoom = zoom - 0.1;
+    width = 100 / zoom;
+    document.body.style.transformOrigin = "left top";
+    document.body.style.transform = "scale(" + zoom + ")";
+    document.body.style.width = width + "%";
+}
+
+
+
+
+
+
+
 function displaySum() {
     let firstNum = Number(document.getElementById('firstNum').innerHTML)
     let secondNum = Number(document.getElementById('secondNum').innerHTML)
@@ -11,3 +37,5 @@ function displaySum() {
   function sendEmail() {
     
   }
+
+
